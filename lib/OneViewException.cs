6b0782f0b37046c3f85b5a7644f3560769dccda1
miqui <migmaqer@gmail.com>
2016-07-13 23:58:58 -0400

    add OneViewException class

---
+++ lib/OneViewException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib
+{
+    public class OneViewException : SdkException
+    {
+        public OneViewException()
+        {
+        }
+
+        public OneViewException(string message) : base(message)
+        {
+        }
+
+        public OneViewException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
